7ee5f67c2e177ad20201a527b776cb27bc8edc5a
asaka <aisk1988@gmail.com>
2015-07-03 11:47:23 +0800

    update

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 var express = require("express");
-var cloud = require('leanengine').Cloud;
+var AV = require('leanengine');
+var expressWs = require('express-ws');
 
+// init LeanEngine
+var PORT = parseInt(process.env.LC_APP_PORT || 3000);
+var APP_ID = process.env.LC_APP_ID;
+var APP_KEY = process.env.LC_APP_KEY;
+var MASTER_KEY = process.env.LC_APP_MASTER_KEY;
+AV.initialize(APP_ID, APP_KEY, MASTER_KEY);
+
+// init websocket
 var app = express();
-require('express-ws')(app);
+expressWs(app);
 app.set('view engine', 'ejs');  
-app.use(cloud);
+app.use(AV.Cloud);
 
 app.get('/', function(req, res) {
   res.render('index');
@@ -15,6 +24,4 @@ app.ws('/echo', function(ws, req) {
     ws.send(msg);
   });
 });
-
-var PORT = parseInt(process.env.LC_APP_PORT || 3000);
 app.listen(PORT);
