95277fa91bad97085879bc9393dde2bc668b17b9
Long Ang <aisk1988@gmail.com>
2016-10-24 16:25:57 +0800

    update project

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ var AV = require('leanengine');
 var expressWs = require('express-ws');
 
 // init LeanEngine
-var PORT = parseInt(process.env.LC_APP_PORT || 3000);
-var APP_ID = process.env.LC_APP_ID;
-var APP_KEY = process.env.LC_APP_KEY;
-var MASTER_KEY = process.env.LC_APP_MASTER_KEY;
+var PORT = parseInt(process.env.LEANCLOUD_APP_PORT || 3000);
+var APP_ID = process.env.LEANCLOUD_APP_ID;
+var APP_KEY = process.env.LEANCLOUD_APP_KEY;
+var MASTER_KEY = process.env.LEANCLOUD_APP_MASTER_KEY;
 AV.initialize(APP_ID, APP_KEY, MASTER_KEY);
 
 // init websocket
